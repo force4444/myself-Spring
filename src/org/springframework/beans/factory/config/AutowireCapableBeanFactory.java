a82d86c61dd2811af2d5c6e85f73d34fe916af36
force4444 <1351438229@qq.com>
2023-10-07 18:36:14 +0800

    容器拓展BeanPostProcessor

--- src/org/springframework/beans/factory/config/AutowireCapableBeanFactory.java
+++ src/org/springframework/beans/factory/config/AutowireCapableBeanFactory.java
@@ -3,4 +3,7 @@ package org.springframework.beans.factory.config;
 import org.springframework.beans.factory.BeanFactory;
 
 public interface AutowireCapableBeanFactory extends BeanFactory {
+    Object applyBeanPostProcessorsBeforeInitialization(Object existingBean, String beanName);
+
+    Object applyBeanPostProcessorsAfterInitialization(Object existingBean, String beanName);
 }
--- src/org/springframework/beans/factory/config/ConfigurableBeanFactory.java
+++ src/org/springframework/beans/factory/config/ConfigurableBeanFactory.java
@@ -3,4 +3,5 @@ package org.springframework.beans.factory.config;
 import org.springframework.beans.factory.HierarchicalBeanFactory;
 
 public interface ConfigurableBeanFactory extends HierarchicalBeanFactory, SingletonBeanRegistry {
+    void addBeanPostProcessor(BeanPostProcessor beanPostProcessor);
 }
