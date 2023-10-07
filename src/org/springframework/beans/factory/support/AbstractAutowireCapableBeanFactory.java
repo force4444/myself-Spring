ec3b1a56e22b59dab3927d8d00a760297d0b779c
force4444 <1351438229@qq.com>
2023-10-07 20:11:12 +0800

    Aware接口

--- src/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -6,6 +6,7 @@ import cn.hutool.core.util.StrUtil;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValue;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
@@ -46,6 +47,9 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
     }
 
     protected Object initializeBean(String beanName, Object bean, BeanDefinition beanDefinition){
+        if (bean instanceof BeanFactoryAware) {
+            ((BeanFactoryAware) bean).setBeanFactory(this);
+        }
         Object wrappedBean = applyBeanPostProcessorsBeforeInitialization(bean, beanName);
 
         try {
